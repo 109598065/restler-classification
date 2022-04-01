65ecb6d211777ff0134671e5f882be8337f4f579
109598065 <t109598065@ntut.org.tw>
2022-04-01 17:00:34 +0800

    update modify path parameter

--- main/grammar/string_modifier.py
+++ main/grammar/string_modifier.py
@@ -30,13 +30,13 @@ class StringModifier:
 
         for index, line in enumerate(lines):
             if state == 0:
-                match_obj_for_search = re.search(r'primitives.restler_static_string\("""', line)
-                if match_obj_for_search:
+                match_obj_for_search_start = re.search(r'primitives.restler_static_string\("""', line)
+                if match_obj_for_search_start:
                     state = 1
             elif state == 1:
-                match_obj_for_search = re.search(r'"(.*)":"""\),', line)
-                if match_obj_for_search:
-                    attribute = match_obj_for_search.group(1)
+                match_obj_for_search_end = re.search(r'"(.*)":"""\),', line)
+                if match_obj_for_search_end:
+                    attribute = match_obj_for_search_end.group(1)
                     state = 2
             elif state == 2:
                 state = 0
@@ -54,14 +54,41 @@ class StringModifier:
 
         for index, line in enumerate(lines):
             if state == 0:
-                match_obj_for_search = re.search(r'primitives.restler_static_string\("/"\),', line)
-                if match_obj_for_search:
+                match_obj_for_search_start = re.search(r'# Endpoint', line)
+                if match_obj_for_search_start:
                     state = 1
+                    pattern = re.compile(r'{(\w+)}')
+                    results = pattern.findall(line)
             elif state == 1:
-                state = 0
+
+                match_obj_for_search_end = re.search(r'req_collection.add_request\(request\)', line)  # todo
+                if match_obj_for_search_end:
+                    state = 0
+
+                match_obj_for_search = re.search(r'restler_static_string\("/"\)', line)
+                if match_obj_for_search:
+                    state = 2
+            elif state == 2:
+
+                match_obj_for_search_end = re.search(r'req_collection.add_request\(request\)', line)  # todo
+                if match_obj_for_search_end:
+                    state = 0
+
+                state = 1
                 match_obj_for_change = re.search(r'restler_fuzzable_string', line)
+                if not results:
+                    state = 0
+                    continue
                 if match_obj_for_change:
-                    line = line.replace('restler_fuzzable_string', 'restler_fuzzable_id')
+                    result = results[0]
+                    results.pop(0)
+                    classify = self._classification.classify(result)
+                    classification = classify if classify else 'string'
+                    line = line.replace('restler_fuzzable_string', 'restler_fuzzable_' + classification)
                     lines[index] = line
+            else:
+                match_obj_for_search_end = re.search(r'req_collection.add_request\(request\)', line)
+                if match_obj_for_search_end:
+                    state = 0
 
         return lines
--- test/grammar/test_string_modifier.py
+++ test/grammar/test_string_modifier.py
@@ -69,15 +69,64 @@ class StringModifierTestCase(unittest.TestCase):
         self.assertEqual('primitives.restler_fuzzable_string("fuzzstring", quoted=True),', lines[5])
 
     def test_modify_path_parameter(self):
-        lines = [line1, line2, line3, line4]
+        line1 = '# Endpoint: /timezone/{area}/{location}/{region}, method: Get'
+        line2 = 'request = requests.Request(['
+        line3 = '    primitives.restler_static_string("GET "),'
+        line4 = '    primitives.restler_basepath("/api/"),'
+        line5 = '    primitives.restler_static_string("/"),'
+        line6 = '    primitives.restler_static_string("timezone"),'
+        line7 = '    primitives.restler_static_string("/"),'
+        line8 = '    primitives.restler_fuzzable_string("fuzzstring", quoted=False),'
+        line9 = '    primitives.restler_static_string("/"),'
+        line10 = '    primitives.restler_fuzzable_string("fuzzstring", quoted=False),'
+        line11 = '    primitives.restler_static_string("/"),'
+        line12 = '    primitives.restler_fuzzable_string("fuzzstring", quoted=False),'
+        line13 = '    primitives.restler_static_string(" HTTP/1.1\r\n"),'
+        line14 = '    primitives.restler_static_string("Accept: application/json\r\n"),'
+        line15 = '    primitives.restler_static_string("Host: worldtimeapi.org\r\n"),'
+        line16 = '    primitives.restler_refreshable_authentication_token("authentication_token_tag"),'
+        line17 = '   primitives.restler_static_string("\r\n"),'
+        line18 = ''
+        line19 = '],'
+        line20 = '   requestId="/timezone/{area}/{location}/{region}"'
+        line21 = ')'
+        line22 = 'req_collection.add_request(request)'
+        lines = [line1, line2, line3, line4, line5, line6, line7, line8, line9, line10, line11, line12, line13, line14, line15, line16, line17, line18, line19, line20, line21, line22]
 
         string_modifier = StringModifier(Similarity(self._classification_table, 0.8))
         lines = string_modifier.modify_path_parameter(lines)
         self.assertTrue(lines)
-        self.assertEqual('primitives.restler_static_string("contributors"),', lines[0])
-        self.assertEqual('primitives.restler_static_string("/"),', lines[1])
-        self.assertEqual('primitives.restler_fuzzable_id("fuzzstring", quoted=False),', lines[2])
-        self.assertEqual('primitives.restler_static_string("/"),', lines[3])
+        self.assertEqual('    primitives.restler_fuzzable_area("fuzzstring", quoted=False),', lines[7])
+        self.assertEqual('    primitives.restler_fuzzable_location("fuzzstring", quoted=False),', lines[9])
+        self.assertEqual('    primitives.restler_fuzzable_region("fuzzstring", quoted=False),', lines[11])
+
+    def test_modify_path_parameter_with_overmuch_parameter(self):
+        line1 = '# Endpoint: /timezone/{area}/{location}.txt, method: Get'
+        line2 = 'request = requests.Request(['
+        line3 = '    primitives.restler_static_string("GET "),'
+        line4 = '    primitives.restler_basepath("/api/"),'
+        line5 = '    primitives.restler_static_string("/"),'
+        line6 = '    primitives.restler_static_string("timezone"),'
+        line7 = '    primitives.restler_static_string("/"),'
+        line8 = '    primitives.restler_fuzzable_string("fuzzstring", quoted=False),'
+        line9 = '    primitives.restler_static_string(" HTTP/1.1\r\n"),'
+        line10 = '    primitives.restler_static_string("Accept: application/json\r\n"),'
+        line11 = '    primitives.restler_static_string("Host: worldtimeapi.org\r\n"),'
+        line12 = '    primitives.restler_refreshable_authentication_token("authentication_token_tag"),'
+        line13 = '    primitives.restler_static_string("\r\n"),'
+        line14 = ''
+        line15 = '],'
+        line16 = '    requestId="/timezone/{area}/{location}.txt"'
+        line17 = ')'
+        line18 = 'req_collection.add_request(request)'
+
+        lines = [line1, line2, line3, line4, line5, line6, line7, line8, line9, line10, line11, line12, line13, line14,
+                 line15, line16, line17, line18]
+
+        string_modifier = StringModifier(Similarity(self._classification_table, 0.8))
+        lines = string_modifier.modify_path_parameter(lines)
+        self.assertTrue(lines)
+        self.assertEqual('    primitives.restler_fuzzable_area("fuzzstring", quoted=False),', lines[7])
 
 
 if __name__ == '__main__':
