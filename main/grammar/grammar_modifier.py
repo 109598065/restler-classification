d2b993935718b54ed7c76888c25933e56330de95
109598065 <t109598065@ntut.org.tw>
2022-04-06 11:22:29 +0800

    update word2vec

--- main/grammar/grammar_modifier.py
+++ main/grammar/grammar_modifier.py
@@ -3,6 +3,7 @@ import configuration
 from main.grammar.file_handling import FileHandling
 from main.grammar.similarity import Similarity
 from main.grammar.string_modifier import StringModifier
+from main.grammar.word2vec import Word2vec
 
 
 class GrammarModifier:
@@ -21,4 +22,18 @@ class GrammarModifier:
         lines = string_modifier.modify_body_parameter(lines)
         lines = string_modifier.modify_path_parameter(lines)
         FileHandling.write(self._file_name, lines)
+
+    # todo
+    def execute_word2vec_similarity(self):
+        classification_table = configuration.string_classification_table
+        lines = FileHandling().read(self._file_name)
+        if not os.path.exists(self._backup_file_name):
+            os.rename(self._file_name, self._backup_file_name)
+
+        string_modifier = StringModifier(Word2vec(classification_table))
+        lines = string_modifier.modify_query_parameter(lines)
+        lines = string_modifier.modify_body_parameter(lines)
+        lines = string_modifier.modify_path_parameter(lines)
+        FileHandling.write(self._file_name, lines)
+
     
