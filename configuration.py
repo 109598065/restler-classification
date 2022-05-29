79b3fd43541a52cfff8cfde4b232cb0659c7ae8f
109598065 <t109598065@ntut.org.tw>
2022-05-29 21:16:39 +0800

    update classification table

--- configuration.py
+++ configuration.py
@@ -3,9 +3,11 @@ string_classification_table = {
     'name': ['name', 'user', 'username', 'contributor', 'artists'],
     'description': ['description', 'summary', 'text', 'title', 'comment', 'value', 'content', 'message'],
     'time_zone': ['time_zone'],
+    'area': ['area'],
+    'location': ['location', 'country', 'address'],
+    'region': ['region'],
     'url': ['url', 'uri', 'link', 'href', 'website'],
     'language': ['language'],
-    'location': ['location', 'country', 'address'],
     'media_type': ['media_type', 'mime_type'],
     'color': ['color'],
     'email': ['email'],
@@ -13,8 +15,8 @@ string_classification_table = {
     'path': ['path'],
     'domain': ['domain'],
     'token': ['token', 'code', 'key'],
-    'category': ['category', 'type', 'genre', 'kind', 'tag'],
+    'category': ['category', 'type', 'genre', 'kind', 'tag', 'output'],
     'ip': ['ip', 'host'],
-    'datetime': ['datetime', 'since', 'until', 'after', 'before', 'time', 'before', 'after', 'min'],
-    'date': ['date', 'due']
+    'string_datetime': ['datetime', 'since', 'until', 'after', 'before', 'time', 'min'],
+    'string_date': ['date', 'due']
 }
