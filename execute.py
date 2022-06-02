ecc5af541c333d18600a686ba10e9908f207f311
109598065 <t109598065@ntut.org.tw>
2022-06-02 22:50:23 +0800

    update execute

--- execute.py
+++ execute.py
@@ -1,16 +1,22 @@
 from pathlib import Path
+from dictionary.main.classification_setter import ClassificationSetter
 from grammar.main.grammar_modifier import GrammarModifier
 
-grammar_path = 'D:\\restler-test\\Compile\\grammar.py'
-grammar_backup_path = grammar_path.replace('.py', '_backup.py')
+path = 'D:\\restler-test-\\Compile'
 similarity_threshold = 0.6
 word2vec_threshold = 0.75
 
+engine_settings = 'engine_settings_classification.json'
+custom_value_gen = 'custom_value_gen_classification.py'
+
 
 if __name__ == '__main__':
-    file_name = Path(grammar_path)
-    backup_file_name = Path(grammar_backup_path)
+    grammar_source = Path(path, 'grammar.py')
+    grammar_target = Path(path, 'grammar_classification.py')
 
-    grammar_modifier = GrammarModifier(file_name, backup_file_name)
-    grammar_modifier.execute_similarity(similarity_threshold)
+    grammar_modifier = GrammarModifier(grammar_source, grammar_target)
+    grammar_modifier.execute_default_similarity(similarity_threshold)
     # grammar_modifier.execute_word2vec_similarity(word2vec_threshold)
+
+    setter = ClassificationSetter(path, engine_settings, custom_value_gen)
+    setter.execute()
